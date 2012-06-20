5a7cecb8c11b2d917ea482ccd00c8cd60e31d1e2
s-soodmand <somaieh.soodmand@gmail.com>
2012-06-20 15:50:09 +0330

    add script galery labkhand but not complate

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+$(function(){
+	$('#ba1').beforeAfter({imagePath:'../script/'});
+	$('#ba2').beforeAfter({imagePath:'../script/'});
+	$('#ba3').beforeAfter({imagePath:'../script/'});
+	$('#ba4').beforeAfter({imagePath:'../script/'});
+	$('#ba5').beforeAfter({imagePath:'../script/'});
+	$('#ba6').beforeAfter({imagePath:'../script/'});
+});
+
+$(document).ready(function() {
+
+	$("a.fancybox").fancybox({
+
+				'overlayShow'	: false,
+
+				'transitionIn'	: 'elastic',
+
+				'transitionOut'	: 'elastic'
+
+			});
+
+
+});
